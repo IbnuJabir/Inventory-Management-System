014e1ab2e4475e716aa2cb6e7b43a11b5bdaf6e3
IbnuJabir <kedirjabir12@gmail.com>
2025-01-02 08:25:07 +0300

    chore: display menu

---
+++ menu.c
@@ -0,0 +1,21 @@
+#include "inventory.h"
+
+void displayMenu(void)
+{
+    printf("\n===== Inventory Management System =====\n");
+    printf("--------------------------------------------------\n");
+    printf("| 1 | Add Category                               |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 2 | Add Product                                |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 3 | Update Product                             |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 4 | Search Product                             |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 5 | List All Products                          |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 6 | List All Categories                        |\n");
+    printf("|---|--------------------------------------------|\n");
+    printf("| 7 | Save and Exit                              |\n");
+    printf("--------------------------------------------------\n");
+}
